5d440462b92a0ab6927baae4f1fd590a5f5e1210
mdy0125 <345661323@qq.com>
2019-07-16 23:54:26 +0800

    Add read line from file sh
    read two lines from /etc/fstab

---
+++ read_line_from_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 从/etc/fstab中读行
+
+File=/etc/fstab
+
+{
+read line1
+read line2
+} < File
+
+echo "First line in $File is:"
+echo "$line1"
+echo
+echo "Second line in $File is:"
+echo "$line2"
+echo
+
+exit 0
